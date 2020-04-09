9901b60b9f2c214ceef1734c47dc2970bb1ce943
firestatus <40839417+nonplated@users.noreply.github.com>
2020-04-09 20:50:19 +0200

    get paper format

--- check_ps.py
+++ check_ps.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
 import os
-
+import math
+from paper_format import get_paper_format
 
 path_file_ps = ''
 path_file_zip = ''
+
 # change your path to archive program
 path_absolute_7z = r"D:\Aplikacje\7-Zip\7z"
 
@@ -24,36 +26,44 @@ def get_values_from(file_content, line):
     lines = [l[len(line):].strip(':').split()
              for l in file_content
              if l[:len(line)] == line]
-    return lines[0] if lines else [] #return only first line (if found more)
+    return lines[0] if lines else []  # return only first line (if found more)
 
 
 def get_page_dimensions(file_content):
     values = get_values_from(file_content, r'%%PageBoundingBox:')
-    divider = 2.8380
+    if len(values) != 4:
+        values = get_values_from(file_content, r'%%BoundingBox:')
     if len(values) != 4:
         return {}
     else:
+        divider = 72 / 25.4  # 72 points / inch[mm]
         return {
-            'width_mm': int(values[2]) / divider,
-            'height_mm': int(values[3]) / divider
+            'width_mm': math.floor((int(values[2])-int(values[0])) / divider),
+            'height_mm': math.floor((int(values[3])-int(values[1])) / divider)
         }
 
 
-def get_output_filename(path_file_ps, width_mm=0, height_mm=0):
-    if width_mm and height_mm:
-        filename = "{}_{}x{}mm.zip".format(
-            '.'.join(
-                path_file_ps.split('.')[:-1]),
+def create_output_filename(path_file_ps, paper_format='', width_mm=0, height_mm=0):
+    if width_mm > 0 and height_mm > 0:
+        filename = "{}_{}x{}mm_{}.zip".format(
+            '.'.join(path_file_ps.split('.')[:-1]),
             round(width_mm),
-            round(height_mm))
+            round(height_mm),
+            paper_format or '')
     else:
         filename = f"{path_file_ps}.zip"
     return filename
 
 
+def marker_eof_exists(file_content):
+    # return file_content[-2] == r'%%EOF'
+    return len([l for l in file_content if l.strip() == r'%%EOF']) == 1
+
+
 if __name__ == "__main__":
     print("Hello, now we will check post-script file (made in 2019)\n")
-    if len(sys.argv) == 2 and len(sys.argv[1]) > 0:
+    print(sys.argv)
+    if len(sys.argv) > 1 and len(sys.argv[1]) > 0:
         path_file_ps = os.path.abspath(sys.argv[1])
         if not os.path.exists(path_file_ps):
             sys.exit(f"ERROR: File not exist: {path_file_ps}")
@@ -65,17 +75,15 @@ if __name__ == "__main__":
     print(f"Loading file: {path_file_ps}")
     file_content = read_file_as_list(path_file_ps)
 
-    # check that finish marker exists at the end of file
-    marker_eof_exists = file_content[len(file_content)-2] == '%'+'%'+'EOF'
-
     # new file name will have dimensions in mm (if found)
     dimensions = get_page_dimensions(file_content)
-    path_file_zip = get_output_filename(path_file_ps, **dimensions)
+    path_file_zip = create_output_filename(
+        path_file_ps, get_paper_format(**dimensions), **dimensions)
 
     if path_file_ps and path_file_zip and path_absolute_7z:
-        if marker_eof_exists:
+        if marker_eof_exists(file_content):
             print('File is correct.')
-            zip_exec_command = f"{path_absolute_7z} a {path_file_zip} {path_file_ps} -sdel"
+            zip_exec_command = f"{path_absolute_7z} a \"{path_file_zip}\" \"{path_file_ps}\" -sdel"
             # used options for 7z:
             #    a      --- add files to archive
             #    -sdel  --- delete source file after success compressing
@@ -84,7 +92,7 @@ if __name__ == "__main__":
             print('ERROR: No matching ending word. File PS may be corrupted.')
             mark_file_as_invalid(path_file_ps, '.invalid')  # change file name
     else:
-        print('Incorrect file names. Please check:')
+        print('Incorrect file names. Please check.')
         print(f"PS file name: {path_file_ps}")
         print(f"Output file name: {path_file_zip}")
         print(f"Path to 7z app: {path_absolute_7z}")
---
+++ paper_format.py
@@ -0,0 +1,38 @@
+
+
+def get_paper_format(width_mm=0, height_mm=0):
+    if width_mm <= 0 or height_mm <= 0:
+        return None
+    # available paper formats
+    PAPER_FORMATS_MM = {
+        'B5': (176.5, 250), 'A4': (210, 297), 'A4E': (236, 323), 'B4': (250, 350),
+        'A3': (297, 420),  'A3E': (323, 446), 'B3': (350, 500),  'A2': (420, 594),
+        'A2E': (445, 619), 'B2': (500, 700), 'B2E': (525, 725), 'A1': (594, 840),
+        'A1E': (619, 865), 'B1': (700, 1000), 'B1E': (725, 1025), 'MAX': (920, 1058)
+    }
+    # calculate area in mm (squared)
+    formats = [{'format': x[0], 's1':x[1][0], 's2':x[1][1], 'area':x[1][0]*x[1][1]}
+               for x in PAPER_FORMATS_MM.items()
+               if (width_mm <= x[1][0] and height_mm <= x[1][1])or(width_mm <= x[1][1] and height_mm <= x[1][0])
+               ]
+    # sorted from smallest to biggest area
+    formats = sorted(formats, key=lambda x: x.get('area'), reverse=False)
+    if len(formats):
+        return formats[0].get('format')  # get the first (the smallest format)
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    assert get_paper_format(210, 297) == 'A4'
+    assert get_paper_format(212, 297) == 'A4E'
+    assert get_paper_format(500, 700) == 'B2'
+    assert get_paper_format(450, 700) == 'B2'
+    assert get_paper_format(400, 400) == 'A2'
+    assert get_paper_format(100, 100) == 'B5'
+    assert get_paper_format(1000, 700) == 'B1'
+    assert get_paper_format(999, 699) == 'B1'
+    assert get_paper_format(700, 999) == 'B1'
+    assert get_paper_format(701, 999) == 'B1E'
+    assert get_paper_format(10, 99999) == None
+    assert get_paper_format(-30, -10) == None
